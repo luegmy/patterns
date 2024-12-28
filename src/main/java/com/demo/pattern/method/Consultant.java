dee3d8398e19619e99a7afbdcd6b12f253fc5910
Jhonny Miguel Neyra Lopez <jneyralo@emeal.nttdata.com>
2024-12-28 17:08:21 -0500

    se actuliza el patron template method

--- src/main/java/com/demo/pattern/method/Consultant.java
+++ src/main/java/com/demo/pattern/method/Consultant.java
@@ -3,25 +3,23 @@ package com.demo.pattern.method;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class Consultant extends StaffPayment{
-    public Consultant(double salaryAmount, double deductionAmount) {
-        super(salaryAmount, deductionAmount);
-    }
+public class Consultant extends StaffPayment {
 
     @Override
     void getSalary() {
-        log.info("Calculating hourly salary.");
+        log.info("Calculating hourly salary. {}:", getSalaryAmount());
     }
 
     @Override
     void getDeduction() {
-        setDeductionAmount(getSalaryAmount()*getDeductionAmount());
+        setDeductionAmount(getSalaryAmount() * getDeductionAmount());
         log.info("5% Deduction of ${} is: ${}.", getSalaryAmount(), getDeductionAmount());
     }
 
     @Override
-    void sendPayment() {
-        log.info("Printing check.");
+    double sendPayment() {
+        log.info("Printing check.{}:", getSalaryAmount() - getDeductionAmount());
+        return getSalaryAmount() - getDeductionAmount();
     }
 
     @Override
--- src/main/java/com/demo/pattern/method/Employee.java
+++ src/main/java/com/demo/pattern/method/Employee.java
@@ -3,24 +3,21 @@ package com.demo.pattern.method;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class Employee extends StaffPayment{
-    public Employee(double salaryAmount, double deductionAmount) {
-        super(salaryAmount, deductionAmount);
-    }
-
+public class Employee extends StaffPayment {
     @Override
     void getSalary() {
-        log.info("Calculating base salary.");
+        log.info("Calculating base salary. {}:", getSalaryAmount());
     }
 
     @Override
     void getDeduction() {
-        setDeductionAmount(getSalaryAmount()*getDeductionAmount());
+        setDeductionAmount(getSalaryAmount() * getDeductionAmount());
         log.info("11% Deduction of ${} is: ${}.", getSalaryAmount(), getDeductionAmount());
     }
 
     @Override
-    void sendPayment() {
-        log.info("Transferring payment to account.");
+    double sendPayment() {
+        log.info("Printing check.{}:", getSalaryAmount() - getDeductionAmount());
+        return getSalaryAmount() - getDeductionAmount();
     }
 }
